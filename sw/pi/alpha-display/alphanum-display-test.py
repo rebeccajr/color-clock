42a1746e2972ce0953cac22021cceaf959e080a1
rebecca <rebeccarashkin@gmail.com>
2022-04-23 20:04:09 -0700

    displays current time, does not update

--- sw/pi/alpha-display/alphanum-display-test.py
+++ sw/pi/alpha-display/alphanum-display-test.py
@@ -1,25 +1,21 @@
 #-------------------------------------------------------------------------------
 # DESCRIPTION
 # This file includes test code for the alpha numeric display backpack purchased
-# frm Adafruit. The data is sent over i2c serial.
+# from Adafruit. The data is sent over i2c serial.
 #-------------------------------------------------------------------------------
 
 import board
 import busio
+import time
 from adafruit_ht16k33.segments import Seg14x4
 
-
-
+from datetime import datetime
 
 if __name__ == "__main__":
 
-    # try to create an i2c device
-    #i2c = busio.I2C(board.SCL, board.SDA)
-    #print('I2C ok!')
-
-
-    i2c = board.I2C()
+    i2c     = board.I2C()
     display = Seg14x4(i2c, address=0x70)
-    display.print_hex(0x1A2B)
-    display.print("gabe")
 
+    the_current_time = datetime.now()
+    str_time = the_current_time.strftime("%H%M")
+    display.print(str_time)
