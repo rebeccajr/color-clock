13ea0b20f9e976fec8cb0cbb7d5d34d7a3b3a2fc
Rebecca Rashkin <rrashkin@ucsc.edu>
2022-05-05 19:34:06 -0700

    added example code for clock module

---
+++ sw/arduino/test.ino/test.ino.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Wire.begin();
+  Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
+}
+
+
+  
+// the loop function runs over and over again forever
+void loop() {
+
+  byte deviceAddress = 0xD0;
+  byte wordAddress   = 0x02;
+  byte hr            = 0x06;
+
+  Wire.beginTransmission(deviceAddress);
+  Wire.endTransmission();
+  
+  //Wire.write(0b0);
+  //Wire.write(wordAddress);
+  //Wire.write(hr);
+  
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(10);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(10);                       // wait for a second
+}
+
+void receiveEvent(int howMany)
+{
+  
+  while(1 < Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read();     // receive byte as a character
+    Serial.print(c);          // print the character
+  }
+  
+  int x = Wire.read();        // receive byte as an integer
+  Serial.println(x);          // print the integer
+  
+}
