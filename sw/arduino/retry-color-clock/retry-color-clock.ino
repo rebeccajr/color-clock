3f3dc76a750e91c458537ad9b1d6938b8e12577c
Rebecca Rashkin <rrashkin@ucsc.edu>
2022-06-18 19:33:30 -0700

    added some test debug

--- sw/arduino/retry-color-clock/retry-color-clock.ino
+++ sw/arduino/retry-color-clock/retry-color-clock.ino
@@ -1,8 +1,22 @@
+
+#include <Arduino.h>
+#include <Wire.h>
+
 #include "logic.hpp"
+#include "debug.hpp"
 
 void setup(){
+  Serial.begin(9600);
+  
+  Wire.begin();
 }
 
 void loop(){
+
+  RgbColor color;
+  Debug::print_new_line();
+  Debug::print_color(color);
+  delay(1000);
 }
+
 
