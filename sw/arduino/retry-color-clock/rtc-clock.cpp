d21caf38b8413d5d6a08a4b2128ae912876b1b56
Rebecca Rashkin <rrashkin@ucsc.edu>
2022-06-21 20:07:57 -0700

    added rtc clock files

---
+++ sw/arduino/retry-color-clock/rtc-clock.cpp
@@ -0,0 +1,7 @@
+//------------------------------------------------------------------------------
+// DESCRIPTION
+// This class contains functionality that interacts with the ChronoDot V2.1
+// RTC.
+//------------------------------------------------------------------------------
+
+#include "rtc-clock.hpp"
---
+++ sw/arduino/retry-color-clock/rtc-clock.hpp
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// DESCRIPTION
+// This class contains functionality that interacts with the ChronoDot V2.1
+// RTC.
+//------------------------------------------------------------------------------
+
+#include <DS3231.h>
+
+class RtcClock{
+
+public:
+  static DS3231 clk;
+
+  static byte rtc_hr;
+  static byte rtc_min;
+  static byte rtc_sec;
+  
+  static void get_rtc_time(byte* the_time, DS3231 clk);
+
+};
