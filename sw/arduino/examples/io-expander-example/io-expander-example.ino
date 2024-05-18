2df75163636d0867a037b09ea15e20034323a12e
rebecca <rebeccarashkin@gmail.com>
2024-05-18 09:24:06 -0700

    add io expander example

---
+++ sw/arduino/examples/io-expander-example/io-expander-example.ino
@@ -0,0 +1,13 @@
+//______________________________________________________________________________
+// This example tests the Adafruit AW923 GPIO expander.
+//______________________________________________________________________________
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_AW9523.h>
+
+void setup()
+{}
+
+void loop()
+{}
